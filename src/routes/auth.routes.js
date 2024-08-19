0f0835fc9dc50204ed9ccfd0d3e05baca637de4a
Rennan Barros <rennan_sbarros@hotmail.com>
2024-08-18 23:09:21 -0300

    feat: implementação da atualização do usuário

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/usuario/authController');
 
 router.post('/registro', userController.registroUsuario);
 router.post('/login', userController.loginUsuario);
-router.get('/usuario/:id', authController.checkToken, authController.rotaPrivada);
+router.get('/usuario/:id', authController.checkToken, userController.usuarioById);
+router.put('/usuario/:id', authController.checkToken, userController.atualizarUsuario);
 
 module.exports = router;
